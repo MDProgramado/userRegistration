a429fd31c22e65c86691aeb6a0e4115b0f14f812
Maicon Douglas <mdprogamacao@outlook.com>
2025-08-25 21:50:23 -0300

    Criando sistema de cadastro em java

--- src/senai/logicaprogramacao/exercicios/exercicio01/UserRegistration.java
+++ src/senai/logicaprogramacao/exercicios/exercicio01/UserRegistration.java
@@ -1,28 +1,57 @@
 package senai.logicaprogramacao.exercicios.exercicio01;
 import java.util.Scanner;
+import java.util.ArrayList;
 
+class User {
+    String names;
+    int age;
+}
 public class UserRegistration {
     public static void main(String[] args) {
-        String userName;
-        int userAge;
-        int amountUsers;
-        int menuOption;
+
         Scanner scanner = new Scanner(System.in);
+        ArrayList<User> listUser = new ArrayList<>();
+        int amountMenu = 0;
 
-        System.out.println("-----------Bém vindo ao sistema de cadastro SENAI-----------------------");
-        System.out.println("Escolha umas das opções do MENU:\n" +
-                "1 – Cadastrar novo usuário\n" +
-                "2 – Listar todos os usuários cadastrados\n" +
-                "3 – Sair do sistema");
 
-        menuOption = scanner.nextInt();
-        switch (menuOption) {
-            case 1:
-                System.out.println("Digite o nome do usuário");
-                userName = scanner.next();
-                System.out.println("Digite a idade do usuário");
-                userAge = scanner.nextInt();
-            break;
+        while (amountMenu != 3) {
+            System.out.println("Bem-vindo ao sistema de cadastro Menu\n");
+            System.out.println("Escolha uma das opções a seguir");
+            System.out.println("1 - Cadastrar pessoa \n2 - Listar pessoas cadastradas \n3 - Encerrar programa \n\n");
+            amountMenu = scanner.nextInt();
+            switch (amountMenu) {
+                case 1:
+                    System.out.println("Digite o nome da pessoa");
+                     String nameDigit = scanner.next();
+                    System.out.println("Digite a idade da pessoa");
+                     int ageDigit = scanner.nextInt();
+                    User newUser = new User();
+                    newUser.names = nameDigit;
+                    newUser.age = ageDigit;
+                    listUser.add(newUser);
+
+                    break;
+                case 2:
+
+                    if (listUser.isEmpty()) {
+                        System.out.println("Nenhuma pessoa cadastrada ainda.");
+                    } else {
+
+                       for (User userMoment : listUser) {
+                           System.out.println("Nome: " + userMoment.names + ", idade: " + userMoment.age);
+                       }
+                    }
+
+                    System.out.println("----------------------------------------------------------------");
+                    break;
+                case 3:
+                    System.out.println("Encerrando programa");
+                    break;
+                default:
+                    System.out.println("Nenhuma opção selecionada. Tente novamente!");
+            }
+
         }
+
     }
 }
